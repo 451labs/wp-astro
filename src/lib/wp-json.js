ae5a5789f28a93b521929b77946dcbdddfc6d37a
Marcel Oomens <mail@marceloomens.com>
2023-01-15 13:56:05 +0100

    Basic api implementatioN
    I’m currently implementing list posts and retrieve post. The list implementation implements pagination as well.
    
    Signed-off-by: Marcel Oomens <mail@marceloomens.com>

---
+++ src/lib/wp-json.js
@@ -0,0 +1,89 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+/**
+ * POSTS
+ */
+
+const POSTS = 'wp/v2/posts/'
+
+export async function listPosts(params, limit) {
+	return await wpList(POSTS, params, limit)
+}
+
+export async function retrievePost(id, params) {
+	return await wpRetrieve(`${POSTS}${id}`, params)
+}
+
+/**
+ * Internal logic
+ */
+ 
+const API_BASE = new URL('wp-json/', process.env.WP_SITE_ADDRESS)
+const PAGE_SIZE = process.env.WP_PER_PAGE_DEFAULT ?? 10
+
+// WARN if PAGE_SIZE > 100 (Wordpress may not honour it)
+
+const headers = {
+ 'Content-Type'	: 'application/json' 
+}
+
+async function wpList(resource, params={}, limit=PAGE_SIZE) { // default to one page
+	const url = new URL(resource, API_BASE)
+	const per_page = limit > 0 && limit < PAGE_SIZE ? limit : PAGE_SIZE
+	for (const [name, value] of Object.entries({ per_page, ...params })) {
+		url.searchParams.set(name, value)
+	}
+
+	console.info('wp-json: LIST', url.href)
+		
+	const response = await fetch(url, { headers })
+	const json = await response.json()
+	if (json.errors) {
+		console.error(json.errors)
+		throw new Error('wp-json: Failed to fetch from Wordpress API')
+	}
+	
+	if (limit > 0 && json.length >= limit) { // && Array.isArray(json)
+		return json.slice(0, limit)
+	}
+		
+	// We need to paginate
+	const TOTAL = response.headers.get('x-wp-total')
+	const TOTAL_PAGES = response.headers.get('x-wp-totalpages')
+	const max = limit <= 0 ? TOTAL : limit
+	
+	for (let i=2; i<=TOTAL_PAGES && json.length < max; i++) {
+		url.searchParams.set('page', i)
+		
+		console.info('wp-json: LIST', url.href)
+		
+		const response = await fetch(url, { headers }) // block scope
+		const next = await response.json()
+		if (next.errors) {
+			console.error(next.errors)
+			throw new Error('wp-json: Failed to fetch from Wordpress API')
+		}
+		
+		json.push(...next)
+	}
+	
+	return json.slice(0, max)
+}
+
+async function wpRetrieve(resource, params={}) {
+	const url = new URL(resource, API_BASE)
+	for (const [name, value] of Object.entries(params)) {
+		url.searchParams.set(name, value)
+	}
+	
+	console.info('wp-json: RETRIEVE', url.href)
+	
+	const response = await fetch(url, { headers })
+	const json = await response.json()
+	if (json.errors) {
+		console.error(json.errors)
+		throw new Error('wp-json: Failed to fetch from Wordpress API')
+	}
+	return json
+}
